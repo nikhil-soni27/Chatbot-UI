420a625e5d0afd88a4594f9ad15662754bf646b6
nikhil-soni27 <nikhilsoni1030@gmail.com>
2021-10-25 19:38:39 +0530

    chat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {MdOutlineKeyboardArrowDown} from "react-icons/md";
+import {FaMicrophone} from "react-icons/all";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="chatbot">
+       <nav className="navbar">
+         <div className="left-side">
+           <div className="circle"></div>
+          <p>Axis AHA!</p>
+         </div>
+         <div className="right-side">
+         <MdOutlineKeyboardArrowDown/>
+         </div>
+       </nav>
+       <div className="right-sender">
+       <div className="sender-side">
+         Fund transfer
+        </div>
+        <div className="chat-circle"></div>
+       </div>
+       <div className="left-receiver">
+        <div className="chat-circle"></div>
+       <div className="receiver-side">
+        Please select benificiary account
+       </div>
+      </div>
+          <div className="right-sender">
+          <div className="sender-side">
+         Person Name
+        </div>
+        <div className="chat-circle"></div>
+       </div>
+       <div className="left-receiver">
+        <div className="chat-circle"></div>
+       <div className="receiver-side">
+        Please enter the amount to transfer
+       </div>
+      </div>
+          <div className="right-sender">
+          <div className="sender-side">
+          Rs.50,000
+        </div>
+        <div className="chat-circle"></div>
+       </div>
+      <div className="left-receiver">
+        <div className="chat-circle"></div>
+       <div className="receiver-side">
+        Please select debit account
+       </div>
+      </div>
+       <form>
+         <input type="text" className="text">
+         </input>
+          <FaMicrophone className="mic"/>
+       </form>
+      </div>
     </div>
   );
 }
 
+
 export default App;
+
